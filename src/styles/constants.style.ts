a7e59d41d2abb45d1d26f4313372dc7fa104eb93
0x29a <cartoonbr495@gmail.com>
2024-04-26 10:41:51 -0400

    Patch: thirdiary color added

--- src/styles/constants.style.ts
+++ src/styles/constants.style.ts
@@ -1,10 +1,12 @@
 export const LegendsColor = {
     textColors: {
         primary: 'white',
-        secondary: 'black'
+        secondary: 'black',
+        thirdiary: 'red'
     },
     backgroundColors: {
         primary: 'black',
-        secondary: 'white'
+        secondary: 'white',
+        thidary: 'red'
     }
 }
