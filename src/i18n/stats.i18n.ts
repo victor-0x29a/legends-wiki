215e4de4441ca94ff8445ef5215df05cb6907e54
0x29a <cartoonbr495@gmail.com>
2024-04-26 08:07:19 -0400

    Feat: i18n for stats

---
+++ src/i18n/stats.i18n.ts
@@ -0,0 +1,32 @@
+export const Stats = {
+    strength: {
+        "en": "Strength",
+        "es": "Fuerza",
+        "pt-br": "Força"
+    },
+    dexterity: {
+        "en": "Dexterity",
+        "es": "Destreza",
+        "pt-br": "Destreza"
+    },
+    magic: {
+        "en": "Magic",
+        "es": "Magia",
+        "pt-br": "Magia"
+    },
+    durability: {
+        "en": "Durability",
+        "es": "Durabilidad",
+        "pt-br": "Durabilidade"
+    },
+    damage: {
+        "en": "Damage",
+        "es": "Daño",
+        "pt-br": "Dano"
+    },
+    protection: {
+        "en": "Protection",
+        "es": "Protección",
+        "pt-br": "Proteção"
+    },
+}
