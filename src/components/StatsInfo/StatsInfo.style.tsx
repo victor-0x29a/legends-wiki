5fc72ba6f056bf888983199de60a9ebb41f30d92
0x29a <cartoonbr495@gmail.com>
2024-04-26 09:05:37 -0400

    Patch: base of style

---
+++ src/components/StatsInfo/StatsInfo.style.tsx
@@ -0,0 +1,13 @@
+import { styled } from "styled-components";
+
+export const StatsContainer = styled.ol`
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    height: auto;
+    padding: 1rem;
+`;
+
+export const StatsItem = styled.li`
+    list-style: circle;
+`
