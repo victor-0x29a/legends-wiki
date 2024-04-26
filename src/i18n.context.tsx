e12fdf176be39268737ad27e1e4c399010d1ff7f
0x29a <cartoonbr495@gmail.com>
2024-04-26 08:14:56 -0400

    Feat: i18n context

---
+++ src/i18n.context.tsx
@@ -0,0 +1,28 @@
+import { createContext, useCallback, useMemo, useState } from "react";
+import { language } from "./types/i18n.type";
+
+type I18nContextValues = {
+    locale: language;
+    onChangeLocale: (newLocale: language) => void;
+}
+
+type I18nProviderProps = {
+    children: React.ReactNode;
+};
+
+const I18nContext = createContext<I18nContextValues>({
+    locale: "pt-br",
+    onChangeLocale: () => { }
+});
+
+export const I18nProvider = ({ children }: I18nProviderProps) => {
+    const [locale, setLocale] = useState<language>("pt-br");
+
+    const onChangeLocale = useCallback((newLocale: language) => setLocale(newLocale), []);
+
+    const values = useMemo(() => ({
+        locale,
+        onChangeLocale
+    }), [locale, onChangeLocale]);
+    return <I18nContext.Provider value={values}>{children}</I18nContext.Provider>;
+};
