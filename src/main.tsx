1b7b272ca51b354fe2668d1e0ae251177b66e858
0x29a <cartoonbr494@gmail.com>
2024-04-26 07:22:01 -0400

    Patch: global style added on project

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { GlobalStyle } from './styles/global.style'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <GlobalStyle/>
     <h1>Hello world!</h1>
   </React.StrictMode>
 )
