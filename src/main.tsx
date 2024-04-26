455862e86c425be1482a37a34d837fa30ef4d939
0x29a <cartoonbr495@gmail.com>
2024-04-26 13:38:26 -0400

    Patch: chakra provider added in root

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { GlobalStyle } from './styles/global.style'
+import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <GlobalStyle/>
-    <h1>Hello world!</h1>
+    <ChakraProvider>
+      <GlobalStyle/>
+      <h1>Hello world!</h1>
+    </ChakraProvider>
   </React.StrictMode>
 )
