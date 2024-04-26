2baf586f552e9b018ed3a145bc3bafaef07e9d8f
0x29a <cartoonbr495@gmail.com>
2024-04-26 08:22:32 -0400

    Feat: StatsInfo component

---
+++ src/components/StatsInfo/index.tsx
@@ -0,0 +1,25 @@
+import { useContext, useMemo } from "react";
+import { StatsContainer, StatsItem } from "./StatsInfo.style";
+import { IStatsInfoProps } from "./StatsInfo.type";
+import { Stats } from "../../i18n/stats.i18n";
+import { I18nContext } from "../../i18n.context";
+
+const StatsInfo = ({ stats }: IStatsInfoProps) => {
+    const { translate } = useContext(I18nContext);
+    const StatsData = useMemo(() => Object.entries(stats).map(([key, value]) => ({
+        key: translate(Stats, key),
+        value
+    })), [stats, translate]);
+
+    return (
+        <StatsContainer>
+            {StatsData.map(({ key, value }, index) => (
+                <StatsItem key={`${key}:${value}-#${index}-${stats}`}>
+                    <b>{key}</b>: {value}
+                </StatsItem>
+            ))}
+        </StatsContainer>
+    )
+}
+
+export default StatsInfo
