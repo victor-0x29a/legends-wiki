89bc87310054610412e571f810895fc764e91b64
0x29a <cartoonbr495@gmail.com>
2024-04-26 08:07:41 -0400

    Patch: react-hooks added in plugins

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,11 +4,12 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-hooks', 'react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    "react-hooks/exhaustive-deps": "warn"
   },
 }
