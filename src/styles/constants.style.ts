f63c882c19d1c078b4c1106fb6deec1e535f23e9
0x29a <cartoonbr495@gmail.com>
2024-04-26 09:03:45 -0400

    Feat: constants for style has added

---
+++ src/styles/constants.style.ts
@@ -0,0 +1,10 @@
+export const LegendsColor = {
+    textColors: {
+        primary: 'white',
+        secondary: 'black'
+    },
+    backgroundColors: {
+        primary: 'black',
+        secondary: 'white'
+    }
+}
