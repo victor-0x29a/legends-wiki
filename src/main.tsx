259fcaf19eb67e1c21ad3074c131cc2b12eb3555
0x29a <cartoonbr495@gmail.com>
2024-04-27 20:31:32 -0400

    Patch: ChakraProvider added on root

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { GlobalStyle } from './styles/global.style'
 import { ChakraProvider } from '@chakra-ui/react'
+import { Item } from './pages/item'
+import { I18nProvider } from './i18n.context'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <GlobalStyle/>
-      <h1>Hello world!</h1>
-    </ChakraProvider>
+    <I18nProvider>
+      <ChakraProvider>
+        <GlobalStyle/>
+        <Item/>
+      </ChakraProvider>
+    </I18nProvider>
   </React.StrictMode>
 )
