a0b3cf184f12a2d740f12526cf516dcaff8ded96
0x29a <cartoonbr495@gmail.com>
2024-04-29 18:53:01 -0400

    Feat: types to entity

---
+++ src/types/entity.type.d.ts
@@ -0,0 +1,25 @@
+type ImageObject = {
+    url: string
+    alt: string
+}
+
+export type Entity = {
+    id: number
+    title: string
+    properties: object
+    description: string
+    author?: string
+    image: null | ImageObject
+    sections: string | null
+    type: string
+}
+
+export type CreateEntity = {
+    title: string
+    properties: object
+    description: string
+    author?: string
+    image: null | ImageObject
+    sections: string | null
+    type: string
+}
