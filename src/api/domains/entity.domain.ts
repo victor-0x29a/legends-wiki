b28e23cf64215a827bc24c889b3dfd46aad895c1
0x29a <cartoonbr495@gmail.com>
2024-04-29 19:00:35 -0400

    Patch: new structure to domains

--- src/api/domains/entity.domain.ts
+++ src/api/domains/entity.domain.ts
@@ -1,10 +1,8 @@
 import { CreateEntity } from "../../types/entity.type";
 import { ServerInstance } from "../instance";
 
-const EntityDomain = ServerInstance
+const Create = (data: CreateEntity) => ServerInstance.post("/entity", data)
 
-const Create = (data: CreateEntity) => EntityDomain.post("/entity", data)
-
-export {
+export const EntityDomain = {
     Create
 }
