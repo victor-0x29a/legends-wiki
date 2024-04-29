3a79680414af5df86645ebeb77730370378b6972
0x29a <cartoonbr495@gmail.com>
2024-04-29 19:59:54 -0400

    Fix: correct data type to a property from props

--- src/components/FormError/FormError.tsx
+++ src/components/FormError/FormError.tsx
@@ -3,10 +3,10 @@ import { useMemo } from "react"
 
 type IFormErrorProps = {
     errorData: string | null | undefined,
-    componentProps: FormHelperTextProps
+    componentProps?: FormHelperTextProps
 }
 
-export const FormError = ({ errorData, componentProps }: IFormErrorProps) => {
+export const FormError = ({ errorData, componentProps = {} }: IFormErrorProps) => {
     const hasError = useMemo(() => Boolean(errorData), [errorData])
     return hasError ? <FormHelperText {...componentProps}>{errorData}</FormHelperText> : ""
 }
