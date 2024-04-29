c1f3ab229cb6f45b145079c58ef1afcb9168fbf8
0x29a <cartoonbr495@gmail.com>
2024-04-29 19:21:15 -0400

    Patch: entity types as array

--- src/pages/Entity/entity.constant.ts
+++ src/pages/Entity/entity.constant.ts
@@ -1,1 +1,11 @@
-export const entityTypes = ["item", "boss", "environment"]
+export const entityTypes = {
+    "item": "Item",
+    "boss": "Boss",
+    "npc": "NPC",
+    "location": "Localização",
+    "quest": "Missão",
+} as {
+    [key: string]: string
+}
+
+export const entityTypesArray = Object.entries(entityTypes).map((data) => data[0])
