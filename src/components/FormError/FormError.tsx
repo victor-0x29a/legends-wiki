38438f3f621a899efb4b3cfa24a2e1884e6c94b7
0x29a <cartoonbr495@gmail.com>
2024-04-29 19:59:20 -0400

    Feat: FormError component

---
+++ src/components/FormError/FormError.tsx
@@ -0,0 +1,12 @@
+import { FormHelperText, FormHelperTextProps } from "@chakra-ui/react"
+import { useMemo } from "react"
+
+type IFormErrorProps = {
+    errorData: string | null | undefined,
+    componentProps: FormHelperTextProps
+}
+
+export const FormError = ({ errorData, componentProps }: IFormErrorProps) => {
+    const hasError = useMemo(() => Boolean(errorData), [errorData])
+    return hasError ? <FormHelperText {...componentProps}>{errorData}</FormHelperText> : ""
+}
