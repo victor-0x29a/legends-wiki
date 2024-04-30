d9f5cd0e7df1bb4929c4035fb47ea679f0ed8b76
0x29a <cartoonbr495@gmail.com>
2024-04-30 08:19:54 -0400

    Patch: correct toast style

--- src/hooks/useAlert.tsx
+++ src/hooks/useAlert.tsx
@@ -10,10 +10,28 @@ export const useAlert = () => {
     const alert = useCallback(({ text, type = "error" }: IAlertProps) => {
         switch (type) {
             case "error":
-                toast.error(text)
+                toast.error(text, {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "dark"
+                })
                 break
             case "success":
-                toast.success(text)
+                toast.success(text, {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "dark"
+                })
                 break
         }
     }, [])
