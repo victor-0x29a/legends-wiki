b0840cdaef947ab9191dbdda8864c1188ad7f4a1
0x29a <cartoonbr495@gmail.com>
2024-04-30 10:59:20 -0400

    Patch: identifing when has unauthorized exception and redirecting to login page

--- src/hooks/useError.tsx
+++ src/hooks/useError.tsx
@@ -1,13 +1,23 @@
 import { useCallback, useContext } from "react"
 import { I18nContext } from "../contexts/i18n.context"
 import { ErrorList } from "../i18n/errors.i18n"
+import { AuthContext } from "../contexts/auth.context"
+import { useAlert } from "./useAlert"
 
 export const useError = () => {
     const { translate } = useContext(I18nContext)
 
+    const { logout } = useContext(AuthContext)
+
+    const { alert } = useAlert()
+
     const translateErrors = useCallback((errorList: string[]) => {
+        if (errorList.includes("Required authentication")) {
+            alert({ text: translate(ErrorList, "Required authentication") })
+            return logout()
+        }
         return errorList.map((error) => translate(ErrorList, error))
-    }, [translate])
+    }, [alert, logout, translate])
 
     return {
         translateErrors
