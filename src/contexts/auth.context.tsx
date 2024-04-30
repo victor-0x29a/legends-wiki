9297e426413a31087f5f4820b540cdc2d33a6658
0x29a <cartoonbr495@gmail.com>
2024-04-30 10:11:45 -0400

    Patch: using localStorage in auth context and returning the decoded token

--- src/contexts/auth.context.tsx
+++ src/contexts/auth.context.tsx
@@ -1,4 +1,5 @@
 import React, { createContext, useCallback, useMemo, useState } from "react";
+import jwt from 'jsonwebtoken'
 
 type IUseAuthProviderProps = {
     children: React.ReactNode
@@ -6,7 +7,12 @@ type IUseAuthProviderProps = {
 
 type IUseAuthValues = {
     token: string | null
-    authenticate: (token: string) => void
+    authenticate: (token: string) => void,
+    logout: () => void,
+    userData: {
+        id: string
+        username: string
+    }
 }
 
 type IUseAuthContext = IUseAuthValues
@@ -15,16 +21,32 @@ type IUseAuthContext = IUseAuthValues
 export const AuthContext = createContext<IUseAuthContext>(null!)
 
 export const AuthProvider = ({ children }: IUseAuthProviderProps) => {
-    const [token, setToken] = useState<null | string>(null)
+    const token = useMemo(() => localStorage.getItem('token'), [])
+
+    const [updatedToken, setUpdatedToken] = useState<string | null>(token)
+
+    const userData = useMemo(() => {
+        if (!updatedToken) return null
+        const decodedToken = jwt.decode(updatedToken)
+        return decodedToken
+    }, [updatedToken]) as { id: string, username: string }
 
     const authenticate = useCallback((newToken: string) => {
-        setToken(newToken)
-    }, [setToken])
+        localStorage.setItem('token', newToken)
+        setUpdatedToken(newToken)
+    }, [])
+
+    const logout = useCallback(() => {
+        localStorage.clear()
+        setUpdatedToken(null)
+    }, [])
 
     const values = useMemo(() => ({
-        token,
-        authenticate
-    }), [token, authenticate])
+        token: updatedToken,
+        authenticate,
+        logout,
+        userData
+    }), [updatedToken, authenticate, logout, userData])
 
     return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>
 }
