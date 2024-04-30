65fa47425b549a70c47f807b92992cc5e6cd3781
0x29a <cartoonbr495@gmail.com>
2024-04-30 10:54:07 -0400

    Feat: generating a new axios instance on each request

--- src/api/domains/user.domain.ts
+++ src/api/domains/user.domain.ts
@@ -1,9 +1,9 @@
 import { signInType } from "../../types/user.type";
-import { ServerInstance } from "../instance";
+import { CreateServerInstance } from "../instance";
 import { extractData } from "../utils/extract-data";
 import { getError } from "../utils/get-error";
 
-const SignIn = (data: signInType) => ServerInstance.post("/user/sign-in", data).then(extractData).catch(getError)
+const SignIn = (data: signInType) => CreateServerInstance().post("/user/sign-in", data).then(extractData).catch(getError)
 
 export const UserDomain = {
     SignIn
