7ce6057e42b3d3b9b6cc8c8491ecb86c0bfe3ea3
0x29a <cartoonbr495@gmail.com>
2024-04-30 07:47:29 -0400

    Feat: errors with i18n

---
+++ src/i18n/errors.i18n.ts
@@ -0,0 +1,44 @@
+import { I18nData } from "../types/i18n.type";
+
+export const ErrorList: I18nData = {
+    "Token not provided": {
+        "en": "Token not provided",
+        "es": "Token no proporcionado",
+        "pt-br": "Token não fornecido"
+    },
+    "Token invalid": {
+        "en": "Token invalid",
+        "es": "Token inválido",
+        "pt-br": "Token inválido"
+    },
+    "User or password invalid.": {
+        "en": "User or password invalid.",
+        "es": "Usuario o contraseña inválidos.",
+        "pt-br": "Usuário ou senha inválidos."
+    },
+    "User already exists by username.": {
+        "en": "User already exists by username.",
+        "es": "Usuario ya existe por nombre de usuario.",
+        "pt-br": "Já existe outro usuário com o mesmo nome de usuário."
+    },
+    "User not found.": {
+        "en": "User not found.",
+        "es": "Usuario no encontrado.",
+        "pt-br": "Usuário não encontrado."
+    },
+    "Entity already exists by title.": {
+        "en": "Entity already exists by title.",
+        "es": "Entidad ya existe por título.",
+        "pt-br": "Já existe uma entidade com o mesmo título."
+    },
+    "Internal error": {
+        "en": "Internal error",
+        "es": "Error interno",
+        "pt-br": "Erro interno"
+    },
+    "Required authentication": {
+        "en": "Required authentication",
+        "es": "Autenticación requerida",
+        "pt-br": "Autenticação necessária"
+    },
+}
