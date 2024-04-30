9c15ba86248d5f30541259661861b255a0bc7ece
0x29a <cartoonbr495@gmail.com>
2024-04-30 08:18:47 -0400

    Feat: new structure to get-error util and applied in user and entity domain

--- src/api/domains/user.domain.ts
+++ src/api/domains/user.domain.ts
@@ -3,9 +3,7 @@ import { ServerInstance } from "../instance";
 import { extractData } from "../utils/extract-data";
 import { getError } from "../utils/get-error";
 
-const SignIn = (data: signInType) => ServerInstance.post("/user/sign-in", data).then(extractData).catch((error) => {
-    throw getError(error)
-})
+const SignIn = (data: signInType) => ServerInstance.post("/user/sign-in", data).then(extractData).catch(getError)
 
 export const UserDomain = {
     SignIn
--- src/api/utils/get-error.ts
+++ src/api/utils/get-error.ts
@@ -3,26 +3,30 @@ import { AxiosError } from "axios";
 export const getError = (error: unknown) => {
     const isAxiosError = error instanceof AxiosError
 
-    if (!isAxiosError) {
-        return ["Internal error"]
-    }
+    const errorIIFE = function() {
+        if (!isAxiosError) {
+            return ["Internal error"]
+        }
 
-    const statusCode = error?.status || 0
+        const statusCode = error?.status || 0
 
-    if (statusCode === 401) {
-        return ["Required authentication"]
-    }
-    if (statusCode >= 500) {
-        return ["Internal error"]
-    }
+        if (statusCode === 401) {
+            return ["Required authentication"]
+        }
+        if (statusCode >= 500) {
+            return ["Internal error"]
+        }
 
-    const responseData = error.response?.data as unknown as {
-        errorList: string[]
-    }
+        const responseData = error.response?.data as unknown as {
+            errorList: string[]
+        }
 
-    if (!responseData) {
-        return ["Internal error"]
-    }
+        if (!responseData) {
+            return ["Internal error"]
+        }
 
-    return responseData?.errorList
+        return responseData?.errorList
+    }()
+
+    throw errorIIFE
 }
