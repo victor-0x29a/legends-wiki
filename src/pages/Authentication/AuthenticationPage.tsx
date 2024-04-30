8a2d82701b98c659593fff411a5b7fe1d5991a4d
0x29a <cartoonbr495@gmail.com>
2024-04-30 08:08:03 -0400

    Feat: calling the backend on submit in authentication page

--- src/pages/Authentication/AuthenticationPage.tsx
+++ src/pages/Authentication/AuthenticationPage.tsx
@@ -1,10 +1,14 @@
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 import { Button, Container, FormControl, FormLabel, Input, InputGroup, InputLeftAddon } from "@chakra-ui/react";
-import { useCallback, useState } from "react";
+import { useCallback, useContext, useState } from "react";
 import { LegendsSize } from "../../styles/constants.style";
 import { useFormik } from "formik";
 import { signInSchema } from "./AuthenticationSchemas";
 import { FormError } from "../../components/FormError/FormError";
+import { UserModel } from "../../api";
+import { AuthContext } from "../../contexts/auth.context";
+import { useError } from "../../hooks/useError";
+import { useAlert } from "../../hooks/useAlert";
 
 const INITIAL_DATA = {
     username: "",
@@ -15,10 +19,23 @@ export const AuthenticationPage = () => {
     const [showPassword, setShowPassword] = useState(false)
     const togglePasswordVisibility = useCallback(() => setShowPassword((curr) => !curr), [])
 
+    const { translateErrors } = useError()
+
+    const { alert } = useAlert()
+
+    const {
+        authenticate
+    } = useContext(AuthContext)
+
     const formik = useFormik({
         initialValues: INITIAL_DATA,
         onSubmit: (values) => {
-            console.log(values)
+            return UserModel.signIn(values).then(({ token }) => {
+                authenticate(token)
+            }).catch((errorList) => {
+                const errors = translateErrors(errorList)
+                errors.forEach((error) => alert({ text: error }))
+            })
         },
         validationSchema: signInSchema
     })
