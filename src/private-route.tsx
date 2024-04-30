fd9ea33e87a5928a8b613cdcbe219b193ef9bc91
0x29a <cartoonbr495@gmail.com>
2024-04-30 08:34:29 -0400

    Feat: private route middleware has created

---
+++ src/private-route.tsx
@@ -0,0 +1,32 @@
+import { useContext, useMemo } from "react"
+import { AuthContext } from "./contexts/auth.context"
+import { Navigate } from "react-router-dom"
+
+type IProtectRouteProps = {
+    children: React.ReactNode
+    forProtect?: boolean
+}
+
+export const PrivateRoute = ({ children, forProtect = false }: IProtectRouteProps) => {
+    const { token } = useContext(AuthContext)
+
+    const hasToken = useMemo(() => Boolean(token), [token])
+
+    /*
+        When is for protect and has token, return children
+        When is for protect and has no token, return to login
+
+        When is not for protect and has token, return to home
+        When is not for protect and has no token, return children
+    */
+
+    if (forProtect) {
+        if (hasToken) return children
+
+        return <Navigate to="/auth/login" />
+    }
+
+    if (hasToken) return <Navigate to="/" />
+
+    return children
+}
