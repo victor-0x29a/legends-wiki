734338998ff0491d7723609582f6916bafa96857
0x29a <cartoonbr495@gmail.com>
2024-04-30 07:49:07 -0400

    Patch: using the getError in UserModel.signIn

--- src/api/models/user.model.ts
+++ src/api/models/user.model.ts
@@ -1,6 +1,7 @@
 import { signInType } from "../../types/user.type";
 import { UserDomain } from "../domains";
+import { getError } from "../utils/get-error";
 
 export const UserModel = {
-    signIn: (data: signInType) => UserDomain.SignIn(data)
+    signIn: (data: signInType) => UserDomain.SignIn(data).catch(getError)
 }
