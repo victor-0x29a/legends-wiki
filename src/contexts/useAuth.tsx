c82922a5850e447aafb96664badcbdbffe0d39f9
0x29a <cartoonbr495@gmail.com>
2024-04-30 07:30:26 -0400

    Feat: useAuth context

---
+++ src/contexts/useAuth.tsx
@@ -0,0 +1,28 @@
+import React, { createContext, useCallback, useMemo, useState } from "react";
+
+type IUseAuthProviderProps = {
+    children: React.ReactNode
+}
+
+type IUseAuthContext = {
+    token: string | null
+    authenticate: (token: string) => void
+} | null
+
+
+const useAuthContext = createContext<IUseAuthContext>(null)
+
+export const useAuthProvider = ({ children }: IUseAuthProviderProps) => {
+    const [token, setToken] = useState<null | string>(null)
+
+    const authenticate = useCallback((newToken: string) => {
+        setToken(newToken)
+    }, [setToken])
+
+    const values = useMemo<IUseAuthContext>(() => ({
+        token,
+        authenticate
+    }), [token, authenticate])
+
+    return <useAuthContext.Provider value={values}>{children}</useAuthContext.Provider>
+}
