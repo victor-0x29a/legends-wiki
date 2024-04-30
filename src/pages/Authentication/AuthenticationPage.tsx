30dd6aac0502c741d6ae0a82d97d329616fa667f
0x29a <cartoonbr495@gmail.com>
2024-04-30 08:25:52 -0400

    Patch: state of loading and form control as form in authentication page

--- src/pages/Authentication/AuthenticationPage.tsx
+++ src/pages/Authentication/AuthenticationPage.tsx
@@ -1,6 +1,6 @@
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 import { Button, Container, FormControl, FormLabel, Input, InputGroup, InputLeftAddon } from "@chakra-ui/react";
-import { useCallback, useContext, useState } from "react";
+import { FormEvent, useCallback, useContext, useState } from "react";
 import { LegendsSize } from "../../styles/constants.style";
 import { useFormik } from "formik";
 import { signInSchema } from "./AuthenticationSchemas";
@@ -16,6 +16,7 @@ const INITIAL_DATA = {
 }
 
 export const AuthenticationPage = () => {
+    const [isLoading, setIsLoading] = useState(false)
     const [showPassword, setShowPassword] = useState(false)
     const togglePasswordVisibility = useCallback(() => setShowPassword((curr) => !curr), [])
 
@@ -30,21 +31,29 @@ export const AuthenticationPage = () => {
     const formik = useFormik({
         initialValues: INITIAL_DATA,
         onSubmit: (values) => {
+            setIsLoading(true)
             return UserModel.signIn(values).then(({ token }) => {
                 authenticate(token)
             }).catch((errorList) => {
                 const errors = translateErrors(errorList)
                 errors.forEach((error) => alert({ text: error }))
+            }).finally(() => {
+                setIsLoading(false)
             })
         },
         validationSchema: signInSchema
     })
 
+    const onSubmit = useCallback((event: FormEvent) => {
+        event.preventDefault()
+        formik.submitForm()
+    }, [formik])
+
     return (
         <Container size={"sm"} w="100vw" h="100vh" display={"flex"} justifyContent={"center"} alignItems={"center"}>
-            <FormControl>
+            <FormControl as="form" onSubmit={onSubmit}>
                 <FormLabel>Usuário</FormLabel>
-                <Input type="text" name="username" onChange={formik.handleChange} onBlur={formik.handleBlur} />
+                <Input type="text" name="username" onChange={formik.handleChange} onBlur={formik.handleBlur} disabled={isLoading} />
                 <FormError errorData={formik.errors.username} />
                 <FormLabel>Senha</FormLabel>
                 <InputGroup>
@@ -53,10 +62,10 @@ export const AuthenticationPage = () => {
                             <ViewOffIcon /> :
                             <ViewIcon />}
                     </InputLeftAddon>
-                    <Input type={showPassword ? "text" : "password"} name="password" onChange={formik.handleChange} onBlur={formik.handleBlur} />
+                    <Input type={showPassword ? "text" : "password"} name="password" onChange={formik.handleChange} onBlur={formik.handleBlur} disabled={isLoading} />
                 </InputGroup>
                 <FormError errorData={formik.errors.password} />
-                <Button w={"100%"} marginTop={LegendsSize.margin.normal} onClick={() => formik.submitForm()} colorScheme="green">
+                <Button w={"100%"} marginTop={LegendsSize.margin.normal} type="submit" colorScheme="green" disabled={isLoading}>
                     Entrar
                 </Button>
             </FormControl>
