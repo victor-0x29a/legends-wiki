5b99ae6806961d8a9180478305c422e0c1c8801b
0x29a <cartoonbr495@gmail.com>
2024-04-30 07:49:54 -0400

    Patch: using getError in EntityModel.Create

--- src/api/models/entity.model.ts
+++ src/api/models/entity.model.ts
@@ -1,7 +1,8 @@
 import { CreateEntity } from "../../types/entity.type";
 import { EntityDomain } from "../domains";
 import { EntityParser } from "../parsers/entity.parser";
+import { getError } from "../utils/get-error";
 
 export const EntityModel = {
-    create: (data: CreateEntity) => EntityDomain.Create(EntityParser.create(data))
+    create: (data: CreateEntity) => EntityDomain.Create(EntityParser.create(data)).catch(getError)
 }
