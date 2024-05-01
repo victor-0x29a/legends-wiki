addbeafd4eb3026d6ef95bd051167cb056094bea
0x29a <cartoonbr495@gmail.com>
2024-05-01 07:19:45 -0400

    Feat: route to list all entities has added in src/api

--- src/api/domains/entity.domain.ts
+++ src/api/domains/entity.domain.ts
@@ -1,9 +1,14 @@
-import { CreateEntity } from "../../types/entity.type";
+import { CreateEntity, EntityFilters } from "../../types/entity.type";
 import { CreateServerInstance } from "../instance";
 import { getError } from "../utils/get-error";
 
 const Create = (data: CreateEntity) => CreateServerInstance().post("/entity", data).catch(getError)
 
+const List = (filters: EntityFilters) => CreateServerInstance().get("/entity", {
+    params: filters
+}).catch(getError)
+
 export const EntityDomain = {
-    Create
+    Create,
+    List
 }
--- src/api/models/entity.model.ts
+++ src/api/models/entity.model.ts
@@ -1,7 +1,8 @@
-import { CreateEntity } from "../../types/entity.type";
+import { CreateEntity, EntityFilters } from "../../types/entity.type";
 import { EntityDomain } from "../domains";
 import { EntityParser } from "../parsers/entity.parser";
 
 export const EntityModel = {
-    create: (data: CreateEntity) => EntityDomain.Create(EntityParser.create(data))
+    create: (data: CreateEntity) => EntityDomain.Create(EntityParser.create(data)),
+    list: (data: EntityFilters) => EntityDomain.List(data)
 }
