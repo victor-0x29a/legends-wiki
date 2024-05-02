35878a726fc64c0ee490476ce7fd4dfc49a0b638
0x29a <cartoonbr495@gmail.com>
2024-05-02 08:17:29 -0400

    Feat: DashboardHeader component has created

---
+++ src/pages/Dashboard/Header.tsx
@@ -0,0 +1,17 @@
+import { ArrowBackIcon } from "@chakra-ui/icons";
+import { Box, Heading } from "@chakra-ui/react";
+import { LegendsSize } from "../../styles/constants.style";
+
+type IHeaderProps = {
+    title: string;
+    onBackClick: () => void;
+}
+
+export const DashboardHeader = ({ title, onBackClick }: IHeaderProps) => {
+    return <Box display={"flex"} w={"100%"} padding={LegendsSize.padding.large} paddingLeft={0} alignItems={"center"}>
+        <ArrowBackIcon marginBottom={LegendsSize.margin.small} marginRight={LegendsSize.margin.normal} onClick={onBackClick} boxSize={8} cursor={"pointer"} />
+        <Heading as={"h1"}>
+            {title}
+        </Heading>
+    </Box>
+}
