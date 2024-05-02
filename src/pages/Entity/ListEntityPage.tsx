89d87fc5a319c944b8dcf8a1c10857897959beda
0x29a <cartoonbr495@gmail.com>
2024-05-02 08:34:45 -0400

    Patch: using EntityTable and button to create an entity

--- src/pages/Entity/ListEntityPage.tsx
+++ src/pages/Entity/ListEntityPage.tsx
@@ -1,11 +1,11 @@
-import { Box, Container, Spinner, Table, Tbody, Text, Th, Thead, Tr } from "@chakra-ui/react";
-import { PaginationBar } from "../../components/PaginationBar/PaginationBar";
+import { Box, Button, Container } from "@chakra-ui/react";
 import { useEntityList } from "./hooks/useEntityList";
-import { LegendsColor, LegendsSize } from "../../styles/constants.style";
-import { useCallback, useMemo } from "react";
-import { DeleteIcon, EditIcon, WarningIcon } from "@chakra-ui/icons";
+import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
-import EntityImage from "../../components/EntityImage/EntityImage";
+import { DashboardHeader } from "../Dashboard/Header";
+import { EntityTable } from "./EntityTable";
+import { AddIcon } from "@chakra-ui/icons";
+import { LegendsSize } from "../../styles/constants.style";
 
 export const ListEntityPage = () => {
     const {
@@ -20,8 +20,6 @@ export const ListEntityPage = () => {
         isLoading
     } = useEntityList()
 
-    const hasEntities = useMemo(() => entityList.length > 0, [entityList])
-
     const Navigate = useNavigate()
 
     const onEditClick = useCallback((id: number) => {
@@ -32,73 +30,31 @@ export const ListEntityPage = () => {
         return () => window.alert("Em breve o modal de confirmação de exclusão será implementado.")
     }, [])
 
+    const onBack = useCallback(() => Navigate("/auth"), [Navigate])
+
+    const onCreate = useCallback(() => Navigate("/entity/create"), [Navigate])
+
     return (
         <Container maxW={"800px"}>
-            <Table>
-                <Thead>
-                    <Tr>
-                        <Th color={LegendsColor.textColors.white}>
-                            ID
-                        </Th>
-                        <Th color={LegendsColor.textColors.white}>
-                            Imagem
-                        </Th>
-                        <Th color={LegendsColor.textColors.white}>
-                            Título
-                        </Th>
-                        <Th color={LegendsColor.textColors.white}>
-                            Ações
-                        </Th>
-                    </Tr>
-                </Thead>
-                <Tbody>
-                    {isLoading && (
-                        <Tr>
-                            <Th colSpan={12} textAlign={"center"}>
-                                <Spinner width={16} height={16} />
-                                <Text marginTop={LegendsSize.margin.normal} fontSize={LegendsSize.fontSize.normal}>
-                                    Carregando entidades
-                                </Text>
-                            </Th>
-                        </Tr>
-                    )}
-                    {!isLoading && entityList.map(({ id, title, image }) => (
-                        <Tr key={`item-${id}-entity-list-page`}>
-                            <Th color={LegendsColor.textColors.gray}>
-                                {id}
-                            </Th>
-                            <Th color={LegendsColor.textColors.gray}>
-                                <EntityImage image={image} />
-                            </Th>
-                            <Th color={LegendsColor.textColors.gray}>
-                                {title}
-                            </Th>
-                            <Th>
-                                <EditIcon onClick={onEditClick(id)} marginRight={LegendsSize.margin.small} cursor={"pointer"} boxSize={5} />
-                                <DeleteIcon onClick={onRemoveClick()} cursor={"pointer"} boxSize={5} />
-                            </Th>
-                        </Tr>
-                    ))}
-                </Tbody>
-            </Table>
-            {!hasEntities && (
-                <Box w={"100%"} textAlign={"center"} marginTop={LegendsSize.margin.normal}>
-                    <Text fontSize={LegendsSize.fontSize.normal}>
-                        <WarningIcon display={"inline"} marginRight={LegendsSize.margin.small} />
-                        Não há entidades para serem exibidas
-                    </Text>
-                </Box>
-            )}
-            {hasEntities && (
-                <PaginationBar
-                    onChangePage={onChangePage}
-                    onChangePerPage={onChangePerPage}
-                    page={page}
-                    perPage={perPage}
-                    totalPages={totalPages}
-                    isLoading={isLoading}
-                />
-            )}
+            <DashboardHeader title="Entidades" onBackClick={onBack} />
+            <Box display={"flex"} marginBottom={LegendsSize.margin.large}>
+                <Button onClick={onCreate} leftIcon={<AddIcon />} colorScheme="green">
+                    Criar entidade
+                </Button>
+            </Box>
+            <EntityTable
+                entityList={entityList}
+                isLoading={isLoading}
+                onEntityEditClick={onEditClick}
+                onEntityRemoveClick={onRemoveClick}
+                paginationProps={{
+                    onChangePage,
+                    onChangePerPage,
+                    page,
+                    perPage,
+                    totalPages
+                }}
+            />
         </Container>
     );
 }
