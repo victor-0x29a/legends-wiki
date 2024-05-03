2e54d889ccbf24074014a84cbb0b2d5abe7a1a7d
0x29a <cartoonbr495@gmail.com>
2024-05-03 09:41:42 -0400

    Feat: entity i18n

---
+++ src/i18n/entity.i18n.ts
@@ -0,0 +1,19 @@
+import { I18nData } from "../types/i18n.type";
+
+export const EntityList: I18nData = {
+    "Colecionáveis.": {
+        "en": "Collectibles.",
+        "es": "Coleccionables.",
+        "pt-br": "Colecionáveis."
+    },
+    "Armadura": {
+        "en": "Armor",
+        "es": "Armadura",
+        "pt-br": "Armadura"
+    },
+    "Bosses": {
+        "en": "Bosses",
+        "es": "Jefes",
+        "pt-br": "Chefes"
+    }
+}
