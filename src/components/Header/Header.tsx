aa74becf02410a2e3b0c58b6124a08e32e3d4189
0x29a <cartoonbr495@gmail.com>
2024-05-03 08:44:45 -0400

    Patch: HeaderSideModal has applied in Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,24 @@
-import { Box, Fade, Image, Slide } from "@chakra-ui/react";
+import { Box, Fade, Image } from "@chakra-ui/react";
 import Logo from "../../assets/logo.png";
 import { LegendsColor, LegendsSize } from "../../styles/constants.style";
 import { HamburgerIcon } from "@chakra-ui/icons";
 import { IconWrapper } from "../IconWrapper/IconWrapper";
-import { useCallback, useState } from "react";
+import { useCallback, useRef, useState } from "react";
+import { HeaderSideModal } from "./HeaderSideModal";
+import { useOutsideClick } from "@chakra-ui/react";
 
 
 export const Header = () => {
     const [isOpenModal, setIsOpenModal] = useState(false)
     const toggleModal = useCallback(() => setIsOpenModal((curr) => !curr), [])
+
+    const modalReference = useRef<HTMLDivElement>()
+
+    useOutsideClick({
+        ref: modalReference as React.RefObject<HTMLElement>,
+        handler: toggleModal,
+        enabled: isOpenModal
+    })
 
     return (
         <Box as="header" w={"100%"} padding={LegendsSize.padding.small} display={"flex"} bgColor={LegendsColor.backgroundColors.secondary} boxShadow={`0 0 10px ${LegendsColor.backgroundColors.secondary}`} justifyContent={"space-between"} alignItems={"center"}>
@@ -18,11 +28,7 @@ export const Header = () => {
                     <HamburgerIcon w={'2rem'} h={'2rem'} cursor={"pointer"} onClick={toggleModal} />
                 </IconWrapper>
             </Fade>
-            <Slide in={isOpenModal} direction="right">
-                <Box display={"flex"} flexDirection={"column"}>
-                    <h1>opened</h1>
-                </Box>
-            </Slide>
+            <HeaderSideModal isOpen={isOpenModal} ref={modalReference as React.RefObject<HTMLElement>} />
         </Box>
     );
 }
