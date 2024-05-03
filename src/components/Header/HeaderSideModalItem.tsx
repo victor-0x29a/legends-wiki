228dfab079a2e9803a149e139f1abad65a08258e
0x29a <cartoonbr495@gmail.com>
2024-05-03 10:38:22 -0400

    Feat: HeaderSideModalItemChild abstracted from HeaderSideModalItem

--- src/components/Header/HeaderSideModalItem.tsx
+++ src/components/Header/HeaderSideModalItem.tsx
@@ -1,45 +1,8 @@
-import { Box, Text, useBoolean } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 import { IHeaderSideModalItemProps } from "./HeaderSideModalItem.type"
-import { LegendsColor, LegendsSize, LegendsValues } from "../../styles/constants.style";
-import { IconWrapper } from "../IconWrapper/IconWrapper";
+import { LegendsColor, LegendsSize } from "../../styles/constants.style";
+import { HeaderSideModalChild } from "./HeaderSideModalItemChild";
 
-const HeaderSideModalChild = ({ onClick, icon, label, isMobile }: {
-    onClick: () => void;
-    icon: React.ReactNode;
-    label: string;
-    isMobile: boolean;
-}) => {
-    const [isHover, setIsHover] = useBoolean()
-    return <Text as="span"
-        onClick={onClick}
-        display={"flex"}
-        color={LegendsColor.textColors.emphasis.secondary}
-        alignItems={"center"}
-        gap={"6px"}
-        paddingLeft={LegendsSize.padding.large}
-        fontSize={LegendsSize.fontSize.large}
-        userSelect={"none"}
-        cursor={"pointer"}
-        transition={`background-color ${LegendsValues.transitionDuration}, color ${LegendsValues.transitionDuration}`}
-        marginTop={LegendsSize.margin.normal}
-        marginBottom={LegendsSize.margin.normal}
-        width={isMobile ? "40%" : "80%"}
-        textAlign={"center"}
-        marginLeft={LegendsSize.margin.normal}
-        borderRadius={LegendsSize.borderRadius.large}
-        _hover={{
-            bgColor: LegendsColor.textColors.emphasis.primary,
-            color: LegendsColor.textColors.white
-        }}
-        onMouseEnter={setIsHover.on}
-        onMouseLeave={setIsHover.off}
-    >
-        <IconWrapper lightMode={!isHover}>
-            {icon}
-        </IconWrapper>
-        {label}
-    </Text>
-}
 
 export const HeaderSideModalItem = ({
     section,
