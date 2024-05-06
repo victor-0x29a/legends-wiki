5d47c3fc9545e30e18e223d855b69e60ec60c990
victor-0x29a <cartoonbr495@gmail.com>
2024-05-06 14:09:34 -0400

    Patch: ListEntityPage has translated

--- src/pages/Entity/ListEntityPage.tsx
+++ src/pages/Entity/ListEntityPage.tsx
@@ -1,11 +1,13 @@
 import { Box, Button, Container } from "@chakra-ui/react";
 import { useEntityList } from "./hooks/useEntityList";
-import { useCallback } from "react";
+import { useCallback, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { DashboardHeader } from "../Dashboard/Header";
 import { EntityTable } from "./EntityTable";
 import { AddIcon } from "@chakra-ui/icons";
 import { LegendsSize } from "../../styles/constants.style";
+import { I18nContext } from "../../contexts/i18n.context";
+import { FormLabels } from "../../i18n/forms.i18n";
 
 export const ListEntityPage = () => {
     const {
@@ -34,12 +36,14 @@ export const ListEntityPage = () => {
 
     const onCreate = useCallback(() => Navigate("/entity/create"), [Navigate])
 
+    const { translate } = useContext(I18nContext)
+
     return (
         <Container maxW={"800px"}>
             <DashboardHeader title="Entidades" onBackClick={onBack} />
             <Box display={"flex"} marginBottom={LegendsSize.margin.large}>
                 <Button onClick={onCreate} leftIcon={<AddIcon />} colorScheme="green">
-                    Criar entidade
+                    {translate(FormLabels, "Create entity")}
                 </Button>
             </Box>
             <EntityTable
