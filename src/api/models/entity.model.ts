e2d4d10939d3aebbf2a2effd9817c0ca09bb7f54
victor-0x29a <cartoonbr495@gmail.com>
2024-05-06 15:09:29 -0400

    Patch: remove method in entity model

--- src/api/models/entity.model.ts
+++ src/api/models/entity.model.ts
@@ -4,5 +4,6 @@ import { EntityParser } from "../parsers/entity.parser";
 
 export const EntityModel = {
     create: (data: CreateEntity) => EntityDomain.Create(EntityParser.create(data)),
-    list: (data: EntityFilters): Promise<ListEntityResponse> => EntityDomain.List(data)
+    list: (data: EntityFilters): Promise<ListEntityResponse> => EntityDomain.List(data),
+    delete: (id: number) => EntityDomain.Delete(id)
 }
