c8d6a78d22075c72a2b9eb3b2a88eae694a19c03
victor-0x29a <cartoonbr495@gmail.com>
2024-05-06 14:31:15 -0400

    Feat: CommonLabels i18n data

---
+++ src/i18n/commonLabels.i18n.ts
@@ -0,0 +1,19 @@
+import { I18nData } from "../types/i18n.type";
+
+export const CommonLabels: I18nData = {
+    "Entities": {
+        "en": "Entites",
+        "es": "Entidades",
+        "pt-br": "Entidades"
+    },
+    "Users": {
+        "en": "Users",
+        "es": "Usuarios",
+        "pt-br": "Usuários"
+    },
+    "My profile": {
+        "en": "My profile",
+        "es": "Mi perfil",
+        "pt-br": "Meu perfil"
+    }
+}
