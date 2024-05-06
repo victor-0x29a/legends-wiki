9fafbd59d014f86b4abddbb8dd5424745ae09b87
victor-0x29a <cartoonbr495@gmail.com>
2024-05-06 13:47:21 -0400

    Feat: i18n file to form labels

---
+++ src/i18n/forms.i18n.ts
@@ -0,0 +1,14 @@
+import { I18nData } from "../types/i18n.type";
+
+export const FormLabels: I18nData = {
+    "Entity created": {
+        "en": "Entity created",
+        "es": "Entidad creada",
+        "pt-br": "Entidade criada"
+    },
+    "Creation of entity": {
+        "en": "Creation of entity",
+        "es": "Creación de entidad",
+        "pt-br": "Criação de entidade"
+    }
+}
