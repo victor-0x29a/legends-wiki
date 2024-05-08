27c845533434c3e201583ea2b0652626db2ec718
victor-0x29a <cartoonbr495@gmail.com>
2024-05-08 11:56:06 -0400

    Patch: parsing the data before request the list of entity

--- src/api/models/entity.model.ts
+++ src/api/models/entity.model.ts
@@ -4,6 +4,6 @@ import { EntityParser } from "../parsers/entity.parser";
 
 export const EntityModel = {
     create: (data: CreateEntity) => EntityDomain.Create(EntityParser.create(data)),
-    list: (data: EntityFilters): Promise<ListEntityResponse> => EntityDomain.List(data),
+    list: (data: EntityFilters): Promise<ListEntityResponse> => EntityDomain.List(EntityParser.list(data)),
     delete: (id: number) => EntityDomain.Delete(id)
 }
