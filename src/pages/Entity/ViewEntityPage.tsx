6b8644b42cacdcf81606d79a34c19c29f33872a4
victor-0x29a <cartoonbr495@gmail.com>
2024-05-14 14:25:28 -0400

    Chore: showing all data information and when is loading show the skeleton

--- src/pages/Entity/ViewEntityPage.tsx
+++ src/pages/Entity/ViewEntityPage.tsx
@@ -1,16 +1,23 @@
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import { Entities } from "../../constants"
 import { NotFound } from "../../not-found"
 import { useEntity } from "./hooks/useEntity"
-import { Box } from "@chakra-ui/react"
+import { Box, Skeleton, SkeletonText } from "@chakra-ui/react"
 import BasicHeader from "../../components/BasicHeader"
 import { LegendsSize } from "../../styles/constants.style"
 import StatsInfo from "../../components/StatsInfo"
 import { IItemStats } from "../../types/item.type"
 import { useMemo } from "react"
 import MDEditor from "@uiw/react-md-editor"
+import { DashboardHeader } from "../Dashboard/Header"
+
+const DEFAULT_CONTAINER_PROPS = {
+    paddingTop: LegendsSize.padding.normal,
+}
 
 export const ViewEntityPage = () => {
+    const Navigate = useNavigate()
+
     const { type, id } = useParams()
 
     const { entity, isLoading } = useEntity(Number(id))
@@ -22,20 +29,40 @@ export const ViewEntityPage = () => {
     }
 
     if (isLoading) {
-        return 'is loading'
+        return <Box {...DEFAULT_CONTAINER_PROPS}>
+            <Box display={"flex"} >
+                <Skeleton height={"50px"} w="100px" h={"100px"} borderRadius={LegendsSize.padding.normal} />
+                <Box>
+                    <Skeleton height={"1rem"} w={"10rem"} marginTop={"1rem"} marginLeft={"0.5rem"} />
+                    <Skeleton height={"1rem"} w={"30rem"} marginTop={"1rem"} marginLeft={"0.5rem"} />
+                </Box>
+            </Box>
+            <Box display={"flex"} gap="1rem" paddingTop={"1rem"} paddingBottom={"1rem"}>
+                {Array.from({ length: 4 }).map((_, index) => (
+                    <li key={`skeleton-stats-info-${index}`} style={{ listStyle: "none" }}>
+                        <Skeleton height={"1rem"} w={"10rem"} />
+                    </li>
+                ))}
+            </Box>
+            <SkeletonText noOfLines={30} lineHeight={"1rem"} />
+        </Box>
     }
 
-    return <Box paddingTop={LegendsSize.padding.normal}>
+    return <Box {...DEFAULT_CONTAINER_PROPS}>
+        <DashboardHeader onBackClick={() => Navigate(-1)} title={"Visualizando"} />
         <BasicHeader
             title={entity?.title}
-            imageDetails={entity?.image}
+            imageDetails={(entity?.image || { src: '', alt: '' })}
             position="right"
-            subtitle={entity.description} />
+            subtitle={entity?.description} />
 
         <StatsInfo
-            stats={entity.properties as unknown as IItemStats}
+            stats={(entity?.properties || {}) as unknown as IItemStats}
             isCentralized={canCenterStats} />
 
-        <MDEditor.Markdown source={entity?.sections || ''} />
+        <MDEditor.Markdown source={entity?.sections || ''} style={{
+            padding: LegendsSize.padding.normal,
+            borderRadius: LegendsSize.borderRadius.normal
+        }} />
     </Box>
 }
