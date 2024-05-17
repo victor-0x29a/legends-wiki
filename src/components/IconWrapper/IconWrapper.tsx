410e5d0002db1073539bb1f3d445ea8b52adb48c
victor-0x29a <cartoonbr495@gmail.com>
2024-05-17 10:32:29 -0400

    Patch: customCss prop to IconWrapper

--- src/components/IconWrapper/IconWrapper.tsx
+++ src/components/IconWrapper/IconWrapper.tsx
@@ -4,8 +4,11 @@ import { ContainerStyled } from "./IconWrapper.style";
 import { IIconWrapperProps } from "./IconWrapper.type";
 import { useMemo } from "react";
 
-export const IconWrapper = ({ children, lightMode = false }: IIconWrapperProps) => {
-    const style = useMemo(() => ContainerStyled(lightMode), [lightMode])
+export const IconWrapper = ({ children, lightMode = false, customCss }: IIconWrapperProps) => {
+    const style = useMemo(() => `
+        ${ContainerStyled(lightMode)}
+        ${customCss ? customCss : ''}
+    `, [customCss, lightMode])
     return (
         <Box css={style as unknown as any}>
             {children}
--- src/components/IconWrapper/IconWrapper.type.ts
+++ src/components/IconWrapper/IconWrapper.type.ts
@@ -1,4 +1,7 @@
+import { RuleSet } from "styled-components";
+
 export type IIconWrapperProps = {
     children: React.ReactNode;
     lightMode?: boolean;
+    customCss?: RuleSet<object>;
 };
