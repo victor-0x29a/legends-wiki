5225b730f6d94474caefffd8301fab5447802ad4
victor-0x29a <cartoonbr495@gmail.com>
2024-05-21 15:57:29 -0400

    Feat: useEditEntity custom hook for editions of entity

---
+++ src/pages/Entity/hooks/useEditEntity.tsx
@@ -0,0 +1,68 @@
+import { useCallback, useContext, useState } from "react"
+import { useEntity } from "./useEntity"
+import { getDifferentKeys } from "../../../api/utils/getDifferentKeys"
+import { FindOneEntity } from "../../../types/entity.type"
+import { useAlert } from "../../../hooks/useAlert"
+import { I18nContext } from "../../../contexts/i18n.context"
+import { CommonLabels } from "../../../i18n/commonLabels.i18n"
+import { EntityModel } from "../../../api"
+import { useNavigate } from "react-router-dom"
+
+interface useEditEntityProps {
+    isLoading: boolean
+    isLoadingEntity: boolean
+    onSubmit: (data: Record<string, unknown>) => void
+    originalData: FindOneEntity
+}
+
+export const useEditEntity = (id: number): useEditEntityProps => {
+    const [isLoading, setIsLoading] = useState(false)
+
+    const Navigate = useNavigate()
+
+    const {
+        alert
+    } = useAlert()
+
+    const {
+        translate
+    } = useContext(I18nContext)
+
+    const {
+        entity: originalData,
+        isLoading: isLoadingEntity
+    } = useEntity(id)
+
+    const Edit = useCallback((data: Record<string, unknown>) => {
+        setIsLoading(true)
+
+        const payload = getDifferentKeys(originalData as unknown as Record<string, unknown>, data)
+
+        const hasPayload = Object.keys(payload).length > 0
+
+        if (!hasPayload) {
+            setIsLoading(false)
+            return alert({
+                text: translate(CommonLabels, "Entity has updated"),
+                type: "success"
+            })
+        }
+
+        EntityModel.edit(Number(id), payload)
+            .then(() => {
+                alert({
+                    text: translate(CommonLabels, "Entity has updated"),
+                    type: "success"
+                })
+                Navigate(-1)
+            })
+            .finally(() => setIsLoading(false))
+    }, [Navigate, alert, id, originalData, translate])
+
+    return {
+        originalData,
+        isLoading,
+        isLoadingEntity,
+        onSubmit: Edit
+    }
+}
