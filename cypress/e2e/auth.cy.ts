cbdfb7489574281876602476a86c2d713ddb5776
Victor <cartoonbr495@gmail.com>
2024-05-23 07:48:54 -0400

    Style: space ident

--- cypress/e2e/auth.cy.ts
+++ cypress/e2e/auth.cy.ts
@@ -48,26 +48,26 @@ describe('auth spec', () => {
         cy.get('body').should('contain', 'Login efetuado')
       })
     })
-  it('log-in with error', () => {
-    cy.visit('/auth')
+    it('log-in with error', () => {
+      cy.visit('/auth')
 
-    cy.fixture('requests.json')
-      .then(({ options }) => {
-        cy.intercept('OPTIONS', '**/user/sign-in', options)
-      })
+      cy.fixture('requests.json')
+        .then(({ options }) => {
+          cy.intercept('OPTIONS', '**/user/sign-in', options)
+        })
 
-    cy.fixture('users.json')
-      .then((usersData) => {
-        const login = usersData.admin
+      cy.fixture('users.json')
+        .then((usersData) => {
+          const login = usersData.admin
 
-        cy.intercept('POST', '**/user/sign-in', login.failedResponse)
+          cy.intercept('POST', '**/user/sign-in', login.failedResponse)
 
-        cy.get('input[name="username"]').type(login.username)
-        cy.get('input[name="password"]').type(`${login.password}.`)
+          cy.get('input[name="username"]').type(login.username)
+          cy.get('input[name="password"]').type(`${login.password}.`)
 
-        cy.get('button').click()
+          cy.get('button').click()
 
-        cy.get('body').should('contain', 'Usuário ou senha incorretos')
-      })
-  })
+          cy.get('body').should('contain', 'Usuário ou senha incorretos')
+        })
+    })
 })
