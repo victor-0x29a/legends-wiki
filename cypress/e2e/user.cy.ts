992579e2abd10de0bec720d09dcba137fe729ec6
Victor <cartoonbr495@gmail.com>
2024-05-24 12:45:21 -0400

    Feat: specs for creation of an user

--- cypress/e2e/user.cy.ts
+++ cypress/e2e/user.cy.ts
@@ -51,6 +51,14 @@ const MockDeleteRequests = ({ id }: { id: number }) => {
         })
 }
 
+const MockCreateRequests = ({ responseIndex = 0 }) => {
+    cy.fixture('requests.json')
+        .then(({ options, users }) => {
+            cy.intercept('OPTIONS', '**/user', options)
+            cy.intercept('POST', '**/user', users.createResponse[responseIndex])
+        })
+}
+
 describe('user table', () => {
     it('should display the user table', () => {
         MockAndVisitTable({ responseIndex: 0 })
@@ -160,5 +168,54 @@ describe('edit user', () => {
         cy.get('button').contains('Editar').click()
 
         cy.get('body').contains('Usuário atualizado')
+    })
+})
+
+describe('create user', () => {
+    it('should create the user', () => {
+        MockAndVisitTable({ responseIndex: 3 })
+
+        MockCreateRequests({ responseIndex: 0 })
+
+        cy.get('button').contains('Criar usu').click()
+
+        cy.get("#user-form-name-input").as("user-input").type("john")
+
+        cy.get("#user-form-username-input").as("username-input").type("johndoe")
+
+        cy.get("#user-form-password-input").as("password-input").type("123456")
+
+        MockTable({ responseIndex: 4 })
+
+        cy.get('button').contains('Criar').click()
+
+        cy.wait(500)
+
+        cy.get('body').contains('Usuário criado')
+
+        cy.get("tbody tr").should("have.length", 1)
+    })
+    it('should create an user without name', () => {
+        MockAndVisitTable({ responseIndex: 5 })
+
+        MockCreateRequests({ responseIndex: 0 })
+
+        cy.get('button').contains('Criar usu').click()
+
+        cy.get("#user-form-username-input").as("username-input").type("johndoe")
+
+        cy.get("#user-form-password-input").as("password-input").type("123456")
+
+        MockTable({ responseIndex: 6 })
+
+        cy.get('button').contains('Criar').click()
+
+        cy.wait(500)
+
+        cy.get('body').contains('Usuário criado')
+
+        cy.get("tbody tr").should("have.length", 1)
+
+        cy.get("tbody tr").first().contains("Desconhecido")
     })
 })
