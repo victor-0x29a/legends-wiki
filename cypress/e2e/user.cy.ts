123240fc05199cf8662a4442630e5b64cf05b68c
Victor <cartoonbr495@gmail.com>
2024-05-24 12:30:06 -0400

    Feat: specs for edit an user

--- cypress/e2e/user.cy.ts
+++ cypress/e2e/user.cy.ts
@@ -99,3 +99,66 @@ describe('delete user', () => {
         cy.get("tbody tr").should("have.length", 1)
     })
 })
+
+describe('edit user', () => {
+    it('should edit the user sending a request', () => {
+        MockFindOneUser({ responseIndex: 0, id: 1 })
+
+        let token = ''
+
+        cy.fixture('users.json')
+            .then(({ admin }) => {
+                token = admin.response.body.token
+            })
+
+        cy.visit('/users/edit/1', {
+            onBeforeLoad(win) {
+                win.localStorage.setItem('token', token)
+            }
+        })
+
+        cy.get("#user-form-name-input").as("user-input").clear()
+        cy.get("@user-input").type("new name")
+
+        MockTable({ responseIndex: 2 })
+
+        cy.fixture('requests.json')
+            .then(({ options }) => {
+                cy.intercept('OPTIONS', '**/user/1', options)
+                cy.intercept('PUT', '**/user/1', {
+                    statusCode: 204
+                })
+            })
+
+        cy.get('button').contains('Editar').click()
+
+        cy.wait(500)
+
+        cy.get('body').contains('Usuário atualizado')
+
+        cy.get("tbody tr").first().contains("new name")
+    })
+
+    it('should not send the request if the form is not changed', () => {
+        MockFindOneUser({ responseIndex: 0, id: 1 })
+
+        MockTable({ responseIndex: 0 })
+
+        let token = ''
+
+        cy.fixture('users.json')
+            .then(({ admin }) => {
+                token = admin.response.body.token
+            })
+
+        cy.visit('/users/edit/1', {
+            onBeforeLoad(win) {
+                win.localStorage.setItem('token', token)
+            }
+        })
+
+        cy.get('button').contains('Editar').click()
+
+        cy.get('body').contains('Usuário atualizado')
+    })
+})
