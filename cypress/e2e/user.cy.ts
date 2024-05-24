3017ebf97f2d682a72ed147d83548b7b2c07ffbc
Victor <cartoonbr495@gmail.com>
2024-05-24 12:10:13 -0400

    Feat: specs for delete modal from user

--- cypress/e2e/user.cy.ts
+++ cypress/e2e/user.cy.ts
@@ -1,3 +1,25 @@
+const MockFindOneUser = ({ responseIndex = 0, id = 1 }) => {
+    cy.fixture('requests.json')
+        .then(({ options, users }) => {
+            cy.intercept('OPTIONS', `**/user/${id}`, options)
+
+            // eslint-disable-next-line prefer-const
+            let data = users.findOneResponse[responseIndex]
+
+            data.body.id = id
+
+            cy.intercept('GET', `**/user/${id}`, data)
+        })
+}
+
+const MockTable = ({ responseIndex = 0 }) => {
+    cy.fixture('requests.json')
+        .then(({ options, users }) => {
+            cy.intercept('OPTIONS', '**/user', options)
+            cy.intercept('GET', '**/user', users.tableResponse[responseIndex])
+        })
+}
+
 const MockAndVisitTable = ({ responseIndex = 0 }) => {
     let token = '';
 
@@ -19,6 +41,16 @@ const MockAndVisitTable = ({ responseIndex = 0 }) => {
         })
 }
 
+const MockDeleteRequests = ({ id }: { id: number }) => {
+    cy.fixture('requests.json')
+        .then(({ options }) => {
+            cy.intercept('OPTIONS', `**/user/${id}`, options)
+            cy.intercept('DELETE', `**/user/${id}`, {
+                statusCode: 204
+            })
+        })
+}
+
 describe('user table', () => {
     it('should display the user table', () => {
         MockAndVisitTable({ responseIndex: 0 })
@@ -35,13 +67,35 @@ describe('user table', () => {
     it('should redirect to the user form when clicking on the edit button', () => {
         MockAndVisitTable({ responseIndex: 0 })
 
+        MockFindOneUser({ responseIndex: 0, id: 1 })
+
         cy.get("tbody tr").first().find("svg").first().click()
-        cy.location('pathname').should('eq', '/users/1')
+        cy.location('pathname').should('eq', '/users/edit/1')
     })
     it('should redirect to the user form when clicking on the create button', () => {
         MockAndVisitTable({ responseIndex: 0 })
 
         cy.get("button").click()
         cy.location('pathname').should('eq', '/users/create')
+    })
+})
+
+describe('delete user', () => {
+    it('should delete the user', () => {
+        MockAndVisitTable({ responseIndex: 0 })
+
+        MockTable({ responseIndex: 1 })
+
+        MockDeleteRequests({ id: 1 })
+
+        cy.get("tbody tr").first().find("svg").last().click()
+
+        cy.get("button").contains("Deletar").click()
+
+        cy.wait(500)
+
+        cy.get('body').contains('Usuário deletado')
+
+        cy.get("tbody tr").should("have.length", 1)
     })
 })
