306787706bd10f97422f7351be2de45ceee2b57c
Victor <cartoonbr495@gmail.com>
2024-05-29 08:48:28 -0400

    Fix: correct event handler for select component

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,6 +1,7 @@
 import { Select as ChakraSelect } from "@chakra-ui/react"
 import { ISelectProps } from "./Select.type"
 import { LegendsColor } from "../../styles/constants.style"
+import { useCallback } from "react"
 
 export const Select = ({
     options,
@@ -9,18 +10,21 @@ export const Select = ({
     size = 'lg',
     others
 }: ISelectProps) => {
+    const handleChange = useCallback((event: React.ChangeEvent<HTMLSelectElement>) => {
+        onSelect(event.target.value)
+    }, [onSelect])
     return (
         <ChakraSelect
             placeholder={placeholder}
             size={size}
             {...others}
             borderColor={LegendsColor.backgroundColors.secondary}
+            onChange={handleChange}
         >
             {options.map(({ value, label }) => (
                 <option
                     key={value}
                     value={value}
-                    onClick={() => onSelect(value)}
                 >
                     {label}
                 </option>
