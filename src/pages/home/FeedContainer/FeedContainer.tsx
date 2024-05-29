19d04beabe6edc0067d1d49eed7276b38cda1592
Victor <cartoonbr495@gmail.com>
2024-05-29 08:04:14 -0400

    Feat: FeedContainer component

---
+++ src/pages/home/FeedContainer/FeedContainer.tsx
@@ -0,0 +1,50 @@
+import { List, ListItem } from "@chakra-ui/react"
+import { IFeedContainerProps } from "./FeedContainer.type"
+import { PaginationBar } from "../../../components/PaginationBar/PaginationBar"
+import { useEntityFeed } from "../hooks/useEntityFeed"
+import { FeedItem } from "../../../components/FeedItem/FeedItem"
+import { ImageObject } from "../../../types/entity.type"
+import { LegendsSize } from "../../../styles/constants.style"
+import { useMemo } from "react"
+
+export const FeedContainer = ({ entityType, onEntityClick }: IFeedContainerProps) => {
+    const {
+        entities,
+        isLoading,
+        page,
+        perPage,
+        totalPages,
+        onChangePage
+    } = useEntityFeed(entityType)
+
+    const canShowPagination = useMemo(() => totalPages > 1, [totalPages])
+
+    return <>
+        <List display={"flex"} flexDirection={"column"}>
+            {entities.map((entity) => (
+                <ListItem
+                    padding={LegendsSize.padding.normal}
+                    key={`feed-item-${entityType}-${entity.id}`}
+                >
+                    <FeedItem
+                        entityId={entity.id}
+                        title={entity.title}
+                        description={entity.description}
+                        image={entity.image as ImageObject}
+                        onClick={onEntityClick}
+                    />
+                </ListItem>
+            ))}
+        </List>
+        {canShowPagination && (
+            <PaginationBar
+                page={page}
+                isLoading={isLoading}
+                perPage={perPage}
+                onChangePage={onChangePage}
+                totalPages={totalPages}
+                onChangePerPage={console.log}
+            />
+        )}
+    </>
+}
---
+++ src/pages/home/FeedContainer/FeedContainer.type.d.ts
@@ -0,0 +1,4 @@
+export interface IFeedContainerProps {
+    entityType: string
+    onEntityClick: (id: string | number) => void
+}
