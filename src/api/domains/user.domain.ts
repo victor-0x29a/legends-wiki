c06b850d19f93dc68c9a1f220227a253015647dc
Victor <cartoonbr495@gmail.com>
2024-05-29 09:02:15 -0400

    Fix: correct schema to create an axios instance

--- src/api/domains/user.domain.ts
+++ src/api/domains/user.domain.ts
@@ -1,19 +1,19 @@
 import { signInPayload, createUserPayload, editUserPayload } from "../../types/user.type";
-import { CreateServerInstance } from "../instance";
+import { AxiosInstance } from "../instance";
 import { extractData } from "../utils/extract-data";
 import { getError } from "../utils/get-error";
 
-const SignIn = (data: signInPayload) => CreateServerInstance().post("/user/sign-in", data).then(extractData).catch(getError)
+const SignIn = (data: signInPayload) => AxiosInstance.post("/user/sign-in", data).then(extractData).catch(getError)
 
-const FindAll = () => CreateServerInstance().get("/user").then(extractData).catch(getError)
+const FindAll = () => AxiosInstance.get("/user").then(extractData).catch(getError)
 
-const Delete = (id: number) => CreateServerInstance().delete(`/user/${id}`).catch(getError)
+const Delete = (id: number) => AxiosInstance.delete(`/user/${id}`).catch(getError)
 
-const FindOne = (id: number) => CreateServerInstance().get(`/user/${id}`).then(extractData).catch(getError)
+const FindOne = (id: number) => AxiosInstance.get(`/user/${id}`).then(extractData).catch(getError)
 
-const Create = (data: createUserPayload) => CreateServerInstance().post("/user", data).then(extractData).catch(getError)
+const Create = (data: createUserPayload) => AxiosInstance.post("/user", data).then(extractData).catch(getError)
 
-const Edit = (id: number, data: editUserPayload) => CreateServerInstance().put(`/user/${id}`, data).catch(getError)
+const Edit = (id: number, data: editUserPayload) => AxiosInstance.put(`/user/${id}`, data).catch(getError)
 
 export const UserDomain = {
     SignIn,
