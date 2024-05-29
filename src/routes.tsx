0260dedc5a948734ee1692cdaf291e7251100f19
Victor <cartoonbr495@gmail.com>
2024-05-29 08:05:32 -0400

    Patch: new i18n labens and adjust on routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -30,7 +30,7 @@ export const router = createBrowserRouter([
     },
     {
         path: ":entityType",
-        element: <Layout>
+        element: <Layout isFreeSizes={true}>
             <EntitiesVisualization />
         </Layout>
     },
