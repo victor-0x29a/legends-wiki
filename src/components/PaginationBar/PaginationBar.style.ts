f71e7dc1a51410f70cf360cafccf036ced86d857
Victor Gomes <121402298+victor-0x29a@users.noreply.github.com>
2024-05-30 07:38:26 -0400

    Style: lint ident

--- src/components/PaginationBar/PaginationBar.style.ts
+++ src/components/PaginationBar/PaginationBar.style.ts
@@ -2,11 +2,11 @@ import { css } from "@emotion/react";
 import { LegendsColor } from "../../styles/constants.style";
 
 export const DefaultPerPageOptionsStyle = css`
-&:hover, &:focus {
-    background-color: ${LegendsColor.textColors.emphasis.primary};
-}
-> option {
-    background-color: transparent;
-    width: 100%;
-}
+    &:hover, &:focus {
+        background-color: ${LegendsColor.textColors.emphasis.primary};
+    }
+    > option {
+        background-color: transparent;
+        width: 100%;
+    }
 `
