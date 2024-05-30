2eedcd48d595015a5ca8cec06b2b525f87c67e3e
Victor <cartoonbr495@gmail.com>
2024-05-30 09:03:33 -0400

    Chore: allowing to run all specs on cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   projectId: 'nxuykk',
   e2e: {
     baseUrl: "http://localhost:5173",
+    experimentalRunAllSpecs: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
