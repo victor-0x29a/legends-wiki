2a504c112b25159d8fb6a85c1f9bebc42f0f5289
0x29a <cartoonbr495@gmail.com>
2024-10-05 20:45:17 -0300

    Feat: new animation for form errrors apresentation

--- src/components/FormError/FormError.tsx
+++ src/components/FormError/FormError.tsx
@@ -3,6 +3,7 @@ import { useContext, useMemo } from "react"
 import { LegendsSize } from "../../styles/constants.style"
 import { I18nContext } from "../../contexts/i18n.context"
 import { FormLabels } from "../../i18n/forms.i18n"
+import "./FormError.scss"
 
 type IFormErrorProps = {
     errorData: string | null | undefined,
@@ -12,13 +13,23 @@ type IFormErrorProps = {
 export const FormError = ({ errorData, componentProps = {} }: IFormErrorProps) => {
     const { translate } = useContext(I18nContext)
     const hasError = useMemo(() => Boolean(errorData), [errorData])
-    return hasError ? (
-        <FormHelperText
-            color={"#FF3333"}
-            marginBottom={LegendsSize.margin.small}
-            {...componentProps}
-        >
-            {translate(FormLabels, errorData!)}
-        </FormHelperText>
-    ) : ""
+
+    return <FormHelperText
+        color={"#FF3333"}
+        marginBottom={LegendsSize.margin.small}
+        className={["form-error", hasError && "form-error__visible"].join(" ")}
+        {...componentProps}
+    >
+        {translate(FormLabels, errorData!)}
+    </FormHelperText>
+    // return hasError ? (
+    //     <FormHelperText
+    //         color={"#FF3333"}
+    //         marginBottom={LegendsSize.margin.small}
+    //         className={"form-error"}
+    //         {...componentProps}
+    //     >
+    //         {translate(FormLabels, errorData!)}
+    //     </FormHelperText>
+    // ) : ""
 }
