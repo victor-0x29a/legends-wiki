b8e75160157ff7efc00d1888a5c8b39e990f3486
Victor W. <cartoonbr495@gmail.com>
2024-10-13 14:09:59 -0300

    Refactor: FormError component

--- src/components/FormError/FormError.tsx
+++ src/components/FormError/FormError.tsx
@@ -1,35 +1,26 @@
-import { FormHelperText, FormHelperTextProps } from "@chakra-ui/react"
+import { FormHelperText } from "@chakra-ui/react"
 import { useContext, useMemo } from "react"
 import { LegendsSize } from "../../styles/constants.style"
 import { I18nContext } from "../../contexts/i18n.context"
 import { FormLabels } from "../../i18n/forms.i18n"
+import type { IFormErrorProps } from './FormError.type'
 import "./FormError.scss"
-
-type IFormErrorProps = {
-    errorData: string | null | undefined,
-    componentProps?: FormHelperTextProps
-}
 
 export const FormError = ({ errorData, componentProps = {} }: IFormErrorProps) => {
     const { translate } = useContext(I18nContext)
     const hasError = useMemo(() => Boolean(errorData), [errorData])
 
+    if (!errorData) return null
+
     return <FormHelperText
         color={"#FF3333"}
         marginBottom={LegendsSize.margin.small}
-        className={["form-error", hasError && "form-error__visible"].join(" ")}
+        className={[
+            "form-error",
+            hasError && "form-error__visible"
+        ].join(" ")}
         {...componentProps}
     >
         {translate(FormLabels, errorData!)}
     </FormHelperText>
-    // return hasError ? (
-    //     <FormHelperText
-    //         color={"#FF3333"}
-    //         marginBottom={LegendsSize.margin.small}
-    //         className={"form-error"}
-    //         {...componentProps}
-    //     >
-    //         {translate(FormLabels, errorData!)}
-    //     </FormHelperText>
-    // ) : ""
 }
---
+++ src/components/FormError/FormError.type.d.ts
@@ -0,0 +1,6 @@
+import { FormHelperTextProps } from '@chakra-ui/react'
+
+export type IFormErrorProps = {
+    errorData: string | null | undefined,
+    componentProps?: FormHelperTextProps
+}
